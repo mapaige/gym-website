0d5efcde2a6360259ccc02fc9156535d777b7345
Marcia Paige <mapaige7@gmail.com>
2020-11-29 14:48:05 -0500

    Corrected transition for mobile menu and add images

--- app.js
+++ app.js
@@ -1,2 +1,14 @@
 const menu = document.querySelector("#mobile-menu")
-const 
+const menuLinks = document.querySelector(".navbar__menu")
+const navLogo = document.querySelector(".navbar__logo")
+const body = document.querySelector("body")
+
+
+// display mobile menu
+const mobileMenu =() => {
+  menu.classList.toggle("is-active")
+  menuLinks.classList.toggle("active")
+  body.classList.toggle("active")
+}
+
+menu.addEventListener("click", mobileMenu)
